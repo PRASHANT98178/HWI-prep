728de73a1a133dcdca002bb65f6c43c4a1efdefb
PRASHANT98178 <115872466+PRASHANT98178@users.noreply.github.com>
2025-06-22 11:46:51 +0530

    Time: 1 ms (99.84%), Space: 44.8 MB (37.5%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -1,29 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    // public List<Integer> copy(List<Integer> arr){
-    //     List<Integer> list = new ArrayList<>();
-    //     for(int ele : arr){
-    //         list.add(ele);
-    //     }
-    //     return list;
-    // }
-    public void helper(TreeNode root, int target,  List<Integer> arr, List<List<Integer>> ans){
+    public static void path(TreeNode root, int target, List<Integer> arr, List<List<Integer>> ans){
         if(root == null) return;
         target -= root.val;
         arr.add(root.val);
-        if(target == 0 && root.left == null && root.right == null ){
-            ans.add(new ArrayList<>(arr));
+        if(target == 0 && root.left == null && root.right == null){
+            ans.add(new ArrayList(arr));
+            
         }
-        // List<Integer> arr1 = copy(arr);
-        // List<Integer> arr2 = copy(arr);
-        helper(root.left, target, arr, ans);
-        helper(root.right, target, arr, ans);
-        arr.remove(arr.size() - 1);
-        
+
+        path(root.left, target, arr, ans);
+        path(root.right, target, arr, ans);
+        arr.remove(arr.size()-1);
     }
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> arr = new ArrayList<>();
-        helper(root, targetSum, arr, ans);
+
+        path(root, targetSum, arr, ans);
         return ans;
     }
 }
