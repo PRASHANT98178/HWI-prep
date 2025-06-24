e19e9e01d6dd5b988922cee966f692721b5b1044
PRASHANT98178 <115872466+PRASHANT98178@users.noreply.github.com>
2025-06-24 11:37:55 +0530

    Time: 5 ms (79.06%), Space: 44.8 MB (60.88%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -1,28 +1,23 @@
 class Solution {
-    
-    public boolean isValid(int i, Set<Integer> set, int k){
-        for(int j : set){
-            if(Math.abs(i-j) <= k) return true;
-        }
-        return false;
-    }
-    
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
         int n = nums.length;
-        Set<Integer> set = new HashSet<>();
-        List<Integer> ans = new ArrayList<>();
+        boolean[] valid = new boolean[n]; 
 
-        for(int i=0; i<n; i++){
-            if(nums[i] == key) set.add(i);
-        }
-
-        for(int i=0; i<n; i++){
-
-            if(isValid(i, set, k)){
-                ans.add(i);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == key) {
+                int start = Math.max(0, i - k);
+                int end = Math.min(n - 1, i + k);
+                for (int j = start; j <= end; j++) {
+                    valid[j] = true;
+                }
             }
         }
-        Collections.sort(ans);
-        return ans;
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (valid[i]) result.add(i);
+        }
+
+        return result;
     }
 }
